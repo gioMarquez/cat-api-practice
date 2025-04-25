e5cfd0683563fe9231ab8abed54f47acb10bbf7c
Jhovanny Fragoso <dev.jhovanny@gmail.com>
2025-04-24 22:56:08 -0600

    Reractor RandomCatPrevisualizer and menu

--- src/components/template/TemplateCat.tsx
+++ src/components/template/TemplateCat.tsx
@@ -1,20 +1,26 @@
 import { OptionMenu } from "../../interfaces/OptionsMenu";
-import { Menu } from "../atoms"
+import { Menu, RandomCatPrevisualizer } from "../atoms"
 import Gallery from "../molecules/Gallery"
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import { useState } from "react";
 
 const TemplateCat = () => {
+    const [option, setOption] = useState<number>(1);
 
     const options: OptionMenu[] = [
         { id: 1, label: "Random", icon: <ShuffleIcon /> },
         { id: 2, label: "Buscar por raza", icon: <LocationSearchingIcon /> },
     ];
 
+    
+
     return (
         <div>
-            <Menu options={options} />
-            <Gallery />
+            <Menu options={options} setOption={setOption} />
+            {option === 1 && <RandomCatPrevisualizer />}
+            {option === 2 && <Gallery />}
+            
         </div>
     )
 }
